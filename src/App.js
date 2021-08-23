540bec3fb4ac0a8a6f6ab694cbd1f3d3c0631cd0
Idan Refaeli <idanrefaeli@Idans-Mac.local>
2021-08-23 11:27:08 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from './pages';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+        <Switch>
+      <Route path='/' exact={true}>
+        <Dashboard></Dashboard>
+      </Route>
+      <Route path='/login'>
+        <Login></Login>
+      </Route>
+      <Route path='*'>
+        <Error></Error>
+      </Route>
+    </Switch>
+    </Router>
+  );
+}
+
+export default App;
